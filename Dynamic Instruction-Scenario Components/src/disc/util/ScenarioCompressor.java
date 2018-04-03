9e8ce5b4d378bcd4bdb3e3f441ce9d4152cf88ef
Liam Williams <liam.a.wms@gmail.com>
2018-04-03 11:39:53 -0400

    Added much-needed utilities to ScenarioCompressor

--- Dynamic Instruction-Scenario Components/src/disc/util/ScenarioCompressor.java
+++ Dynamic Instruction-Scenario Components/src/disc/util/ScenarioCompressor.java
@@ -30,6 +30,27 @@ public class ScenarioCompressor {
 	}
 	
 	/**
+	 * Constructor that immediately reads the given file into memory.
+	 * 
+	 * @param input The input file to be read
+	 * @throws FileNotFoundException If no such file exists
+	 */
+	public ScenarioCompressor(File input) throws FileNotFoundException {
+		scenarios = new ArrayList<Scenario>(0);
+		this.decompress(input);
+	}
+	
+	/**
+	 * Constructor that immediately reads the given String into internal data.
+	 * 
+	 * @param input The input String to be read
+	 */
+	public ScenarioCompressor(String input) {
+		scenarios = new ArrayList<Scenario>(0);
+		this.decompress(input);
+	}
+	
+	/**
 	 * Constructor primarily for the clone() method
 	 */
 	public ScenarioCompressor(ArrayList<Scenario> scenarios) {
@@ -44,13 +65,7 @@ public class ScenarioCompressor {
 	 */
 	public void compress(File output) throws FileNotFoundException {
 		PrintWriter op = new PrintWriter(output);
-		op.write("#! BEGIN SCENARIO CHAIN\r\n");
-		Scenario[] scene = this.getScenarios();
-		for(Scenario s : scene) {
-			op.println(s.toString());
-			op.print("#!\r\n");
-		}
-		op.print("#END SCENARIO CHAIN");
+		op.write(this.toString());
 		op.close();
 	}
 	
@@ -71,14 +86,13 @@ public class ScenarioCompressor {
 	}
 	
 	/**
-	 * Decompresses the given file into the internal array. Will not clear the internal array.
+	 * Decompresses the given file into the internal array. Will clear the internal array.
 	 * 
 	 * @param input The file to be decompressed
 	 * @throws FileNotFoundException If the file does not exist or cannot be accessed
 	 */
-	public void decompress(File input) throws FileNotFoundException {
+	public void decompress(Scanner scn) {
 		this.clearScenarioList();
-		Scanner scn = new Scanner(input);
 		StringBuilder sb = new StringBuilder("");
 		while(scn.hasNextLine()) {
 			String tmp = scn.nextLine().trim();
@@ -89,6 +103,26 @@ public class ScenarioCompressor {
 			else sb.append(tmp + "\r\n");
 		}
 		scn.close();
+		this.clean();
+	}
+	
+	/**
+	 * "Decompresses" the given file into the internal array. Will clear the internal array.
+	 * 
+	 * @param input The file to be read
+	 * @throws FileNotFoundException If the file does not exist or cannot be accessed
+	 */
+	public void decompress(File input) throws FileNotFoundException {
+		decompress(new Scanner(input));
+	}
+	
+	/**
+	 * "Decompresses" the given String into the internal array. Will clear the internal array.
+	 * 
+	 * @param input The String to be read
+	 */
+	public void decompress(String input) {
+		decompress(new Scanner(input));
 	}
 	
 	/**
@@ -151,6 +185,47 @@ public class ScenarioCompressor {
 		Queue<Scenario> toReturn = new LinkedList<Scenario>();
 		for(Scenario s : this.getScenarios()) toReturn.add(s);
 		return toReturn;
+	}
+	
+	/**
+	 * Removes any empty scenarios that may have been accidentally added during parsing of a file.
+	 */
+	private void clean() {
+		Scenario tmp = null;
+		tmp = this.getScenarioByName(null);
+		if(tmp != null && tmp.getArgs().length < 1) {
+			scenarios.remove(tmp);
+			this.clean();
+		}
+	}
+	
+	/**
+	 * @return The String representation of the {@link Scenario}s in this object, as they would be seen in a file.
+	 */
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder("");
+		sb.append("#! BEGIN SCENARIO CHAIN\r\n");
+		Scenario[] scene = this.getScenarios();
+		for(Scenario s : scene) {
+			sb.append(s.toString() + "\r\n");
+			sb.append("#!\r\n");
+		}
+		sb.append("#END SCENARIO CHAIN");
+		return sb.toString();
+	}
+	
+	@Override
+	public int hashCode() {
+		Scenario[] op = this.getScenarios();
+		long toOut = 0;
+		for(Scenario s : op) toOut += s.hashCode();
+		return (int) Math.pow(toOut, (1 / op.length));
+	}
+	
+	@Override
+	public ScenarioCompressor clone() {
+		return new ScenarioCompressor(this.scenarios);
 	}
 	
 }
