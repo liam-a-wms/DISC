c1fdad282bd2d3df75d5936519b98a69cf71ad85
Liam Williams <liam.a.wms@gmail.com>
2018-04-24 20:59:50 -0400

    Added more comments.

--- Dynamic Instruction-Scenario Components/src/disc/tools/Directory.java
+++ Dynamic Instruction-Scenario Components/src/disc/tools/Directory.java
@@ -5,6 +5,15 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Stores the object references (and by extension, methods) to be made available
+ * to the {@link Interpreter}. Can work using Class names as the reference names
+ * or nicknames (what the specific instance is called). Can hold multiple
+ * instances of the same object type if nicknames are used.
+ * 
+ * @author Liam Williams
+ * @version 1.0.0
+ */
 public class Directory {
 
     Map<String, ObjectData> m = new HashMap<>();
@@ -12,7 +21,8 @@ public class Directory {
     /**
      * Adds references to each object and its parameters.
      * 
-     * @param objs Object references.
+     * @param objs
+     *            Object references
      */
     public Directory(Object... objs) {
         for(int i = 0; i < objs.length; i++) {
@@ -20,6 +30,15 @@ public class Directory {
         }
     }
 
+    /**
+     * Adds references to each object, with custom names to match them, and
+     * their parameters. Indexes should align for associated things.
+     * 
+     * @param names
+     *            The names to associate to each object
+     * @param objs
+     *            Object references
+     */
     public Directory(String[] names, Object[] objs) {
         for(int i = 0; i < ((names.length + objs.length) / 2); i++) {
             m.put(names[i], new ObjectData(objs[i]));
@@ -27,25 +46,54 @@ public class Directory {
     }
 
     /**
-     * Adds a reference after initialization
+     * Adds a reference after initialization. Uses the class's name.
+     * 
      * @param obj
      */
     public void addObjectReference(Object obj) {
         m.put(obj.getClass().getName(), new ObjectData(obj));
     }
 
+    /**
+     * Adds a reference with a nickname after initialization.
+     * 
+     * @param name
+     * @param obj
+     */
     public void addObjectReference(String name, Object obj) {
         m.put(name, new ObjectData(obj));
     }
 
+    /**
+     * Looks up the given name/nickname in the {@link Directory}
+     * 
+     * @param className
+     * @return
+     */
     public Object lookupObject(String className) {
         return m.get(className).classInstance;
     }
 
+    /**
+     * Looks up the methods of the object with the given name/nickname in the
+     * {@link Directory}.
+     * 
+     * @param className
+     * @return
+     */
     public Method[] lookupObjectMethods(String className) {
         return m.get(className).methods;
     }
 
+    /**
+     * Looks up the specific method of the specific object by name/nickname in
+     * the {@link Directory}
+     * 
+     * @param className
+     * @param methodName
+     * @param numOfParameters
+     * @return null if the method was not found
+     */
     public Method lookupMethod(String className, String methodName,
             int numOfParameters) {
         if(m.get(className) == null) return null;
@@ -56,17 +104,38 @@ public class Directory {
                 .findAny().orElse(tryDumbLookup(ms, methodName));
     }
 
+    /**
+     * Tries a lookup without parameter count if the other lookupMethod fails.
+     * 
+     * @param ms
+     *            An array of the methods of the object
+     * @param methodName
+     * @return null if the method was not found
+     */
     private Method tryDumbLookup(Method[] ms, String methodName) {
         return Arrays.stream(ms).filter(m1 -> m1.getName().equals(methodName))
                 .findAny().orElse(null);
     }
 }
 
+/**
+ * Helper class that stores information on the object reference in the
+ * {@link Directory}.
+ * 
+ * @author Liam
+ * @version 1.0.0
+ */
 class ObjectData {
 
     public Object classInstance;
     public Method[] methods;
 
+    /**
+     * Creates an {@link ObjectData} object using the given object reference.
+     * 
+     * @param obj
+     *            An object instance reference
+     */
     public ObjectData(Object obj) {
         classInstance = obj;
         methods = obj.getClass().getDeclaredMethods();
--- Dynamic Instruction-Scenario Components/src/disc/tools/Interpreter.java
+++ Dynamic Instruction-Scenario Components/src/disc/tools/Interpreter.java
@@ -14,21 +14,22 @@ import disc.data.Instruction;
 import disc.data.Scenario;
 
 /**
- * Dynamic interpreter that can run an entire {@link Scenario} using method lookups
- * and a variable heaps. Can handle a variable return from a method and then variable
- * substitution in a later {@link Instruction}. Defaults to discarding the method return
- * unless the Instruction specifically has a "return" arg in it giving the variable a name
- * (eg "return number"). If objects are being passed around as variables then lossless 
- * serialization (using the toString() method) and deserialization (using a Constructor
- * with a String as the only argument) is expected.
- * <br>
- * The Interpreter uses two Threads. One for the Interpreter itself (which only happens if you
- * properly call start() instead of run()) and one for handling each Instruction. If an 
- * Instruction or called method causes an error or Exception, the Thread will stop and the
- * next Instruction will be picked up and handled by a new Thread.
+ * Dynamic interpreter that can run an entire {@link Scenario} using method
+ * lookups and a variable heaps. Can handle a variable return from a method and
+ * then variable substitution in a later {@link Instruction}. Defaults to
+ * discarding the method return unless the Instruction specifically has a
+ * "return" arg in it giving the variable a name (eg "return number"). If
+ * objects are being passed around as variables then lossless serialization
+ * (using the toString() method) and deserialization (using a Constructor with a
+ * String as the only argument) is expected. <br>
+ * The Interpreter uses two Threads. One for the Interpreter itself (which only
+ * happens if you properly call start() instead of run()) and one for handling
+ * each Instruction. If an Instruction or called method causes an error or
+ * Exception, the Thread will stop and the next Instruction will be picked up
+ * and handled by a new Thread.
  * 
  * @author Liam Williams
- * @version 0.1.0
+ * @version 0.1.1
  */
 public class Interpreter extends Thread {
 
@@ -44,8 +45,10 @@ public class Interpreter extends Thread {
      * Instantiates a new {@link Interpreter} with the given {@link Directory}
      * to execute the given {@link Scenario}.
      * 
-     * @param dir The Directory to lookup methods from
-     * @param scenario The Scenario to execute
+     * @param dir
+     *            The Directory to lookup methods from
+     * @param scenario
+     *            The Scenario to execute
      */
     public Interpreter(Directory dir, Scenario scenario) {
         this.dir = dir;
@@ -83,7 +86,7 @@ public class Interpreter extends Thread {
 }
 
 /**
- * Given the {@link Instruction}, {@link Directory}, and active heap of the 
+ * Given the {@link Instruction}, {@link Directory}, and active heap of the
  * {@link Interpreter}, reads the Instruction's data and acts on it. Uses the
  * Directory to look up a method and execute it with the given args. Halts if
  * the method doesn't exist or another problem arises, which will cause the
@@ -102,9 +105,13 @@ class InstructionHandler implements Runnable {
     /**
      * Creates a new {@link InstructionHandler}.
      * 
-     * @param toExecute The {@link Instruction} to read and act on
-     * @param dir The {@link Directory} to use methods from
-     * @param heap The heap to store data in (It's actually a Map but don't worry about it)
+     * @param toExecute
+     *            The {@link Instruction} to read and act on
+     * @param dir
+     *            The {@link Directory} to use methods from
+     * @param heap
+     *            The heap to store data in (It's actually a Map but don't worry
+     *            about it)
      */
     public InstructionHandler(Instruction toExecute, Directory dir,
             Map<String, String> heap) {
@@ -142,12 +149,15 @@ class InstructionHandler implements Runnable {
     }
 
     /**
-     * Reads the parameter types of the given method and converts the arg
-     * array to match. Assumes correct count.
+     * Reads the parameter types of the given method and converts the arg array
+     * to match. Assumes correct count.
      * 
-     * @param args The args array to convert
-     * @param m The method to source types from
-     * @return The converted array, with matching types to the method's parameter types
+     * @param args
+     *            The args array to convert
+     * @param m
+     *            The method to source types from
+     * @return The converted array, with matching types to the method's
+     *         parameter types
      */
     private Object[] parseArgs(String[] args, Method m) {
         Class<?>[] c = m.getParameterTypes();
@@ -172,7 +182,8 @@ class InstructionHandler implements Runnable {
                 o[i] = Float.valueOf(args[i]);
             } else {
                 try {
-                    o[i] = c[i].getDeclaredConstructor(String.class).newInstance(args[i]);
+                    o[i] = c[i].getDeclaredConstructor(String.class)
+                            .newInstance(args[i]);
                 } catch(InstantiationException | IllegalAccessException
                         | IllegalArgumentException | InvocationTargetException
                         | NoSuchMethodException | SecurityException e) {
@@ -186,7 +197,8 @@ class InstructionHandler implements Runnable {
     /**
      * Checks the variable storage heap for a matching name.
      * 
-     * @param varName The name to look for
+     * @param varName
+     *            The name to look for
      * @return A true or false value indicating whether or not the key exists
      */
     private boolean checkInHeap(String varName) {
@@ -196,7 +208,8 @@ class InstructionHandler implements Runnable {
     /**
      * Grabs the name of the variable from an arg containing the word "return"
      * 
-     * @param arg0 A return arg to be parsed
+     * @param arg0
+     *            A return arg to be parsed
      * @return The name of the expected return key
      */
     private String parseVar(String arg0) {
