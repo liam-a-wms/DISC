f36477e028c003d723e3785969256b564c007650
Liam Williams <liam.a.wms@gmail.com>
2018-04-24 20:50:03 -0400

    Added the rest of the comments for the Interpreter

--- Dynamic Instruction-Scenario Components/src/disc/tools/Interpreter.java
+++ Dynamic Instruction-Scenario Components/src/disc/tools/Interpreter.java
@@ -13,6 +13,23 @@ import java.util.concurrent.Future;
 import disc.data.Instruction;
 import disc.data.Scenario;
 
+/**
+ * Dynamic interpreter that can run an entire {@link Scenario} using method lookups
+ * and a variable heaps. Can handle a variable return from a method and then variable
+ * substitution in a later {@link Instruction}. Defaults to discarding the method return
+ * unless the Instruction specifically has a "return" arg in it giving the variable a name
+ * (eg "return number"). If objects are being passed around as variables then lossless 
+ * serialization (using the toString() method) and deserialization (using a Constructor
+ * with a String as the only argument) is expected.
+ * <br>
+ * The Interpreter uses two Threads. One for the Interpreter itself (which only happens if you
+ * properly call start() instead of run()) and one for handling each Instruction. If an 
+ * Instruction or called method causes an error or Exception, the Thread will stop and the
+ * next Instruction will be picked up and handled by a new Thread.
+ * 
+ * @author Liam Williams
+ * @version 0.1.0
+ */
 public class Interpreter extends Thread {
 
     protected ExecutorService executor = Executors.newSingleThreadExecutor();
@@ -23,29 +40,27 @@ public class Interpreter extends Thread {
     protected Map<String, String> heap = new HashMap<>();
     protected long timeout = 1000;
 
-    public Interpreter(Directory dir) {
-        this.dir = dir;
-    }
-
     /**
-     * Please use this one, it's the only one I've tested.<br>
-     * Uses the fully qualified class name when referring to objects. Not fully
-     * typesafe when calling methods.
+     * Instantiates a new {@link Interpreter} with the given {@link Directory}
+     * to execute the given {@link Scenario}.
      * 
-     * @param dir
-     * @param scenario
+     * @param dir The Directory to lookup methods from
+     * @param scenario The Scenario to execute
      */
     public Interpreter(Directory dir, Scenario scenario) {
         this.dir = dir;
         this.work = scenario;
     }
 
+    /**
+     * Pulls the Queue of {@link Instruction}s from the {@link Scenario}.
+     */
     private void init() {
         q = work.getInstructionQueue();
     }
 
     /**
-     * Use start() to have this method called inside its own thread, runs the
+     * Use start() to have this method called inside its own thread; runs the
      * interpreter.
      */
     public void run() {
@@ -67,12 +82,30 @@ public class Interpreter extends Thread {
 
 }
 
+/**
+ * Given the {@link Instruction}, {@link Directory}, and active heap of the 
+ * {@link Interpreter}, reads the Instruction's data and acts on it. Uses the
+ * Directory to look up a method and execute it with the given args. Halts if
+ * the method doesn't exist or another problem arises, which will cause the
+ * Interpreter to move on to the next Instruction by calling a new
+ * InstructionHandler.
+ * 
+ * @author Liam Williams
+ * @version 1.0.0
+ */
 class InstructionHandler implements Runnable {
 
     Instruction inst;
     Directory dir;
     Map<String, String> heap;
 
+    /**
+     * Creates a new {@link InstructionHandler}.
+     * 
+     * @param toExecute The {@link Instruction} to read and act on
+     * @param dir The {@link Directory} to use methods from
+     * @param heap The heap to store data in (It's actually a Map but don't worry about it)
+     */
     public InstructionHandler(Instruction toExecute, Directory dir,
             Map<String, String> heap) {
         inst = toExecute;
@@ -108,6 +141,14 @@ class InstructionHandler implements Runnable {
         }
     }
 
+    /**
+     * Reads the parameter types of the given method and converts the arg
+     * array to match. Assumes correct count.
+     * 
+     * @param args The args array to convert
+     * @param m The method to source types from
+     * @return The converted array, with matching types to the method's parameter types
+     */
     private Object[] parseArgs(String[] args, Method m) {
         Class<?>[] c = m.getParameterTypes();
         Object[] o = new Object[c.length];
@@ -142,12 +183,24 @@ class InstructionHandler implements Runnable {
         return o;
     }
 
+    /**
+     * Checks the variable storage heap for a matching name.
+     * 
+     * @param varName The name to look for
+     * @return A true or false value indicating whether or not the key exists
+     */
     private boolean checkInHeap(String varName) {
         return heap.keySet().stream().anyMatch(s -> s.equals(varName));
     }
 
+    /**
+     * Grabs the name of the variable from an arg containing the word "return"
+     * 
+     * @param arg0 A return arg to be parsed
+     * @return The name of the expected return key
+     */
     private String parseVar(String arg0) {
-        return arg0.substring(6).trim();
+        return arg0.trim().substring(6).trim();
     }
 
 }
