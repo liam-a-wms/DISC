0909e7f9cbef593ba87a5acf9bdf2a8b66a8ee65
Liam Williams <liam.a.wms@gmail.com>
2018-07-25 09:51:07 -0400

    Added package-info files.

---
+++ Dynamic Instruction-Scenario Components/src/disc/data/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data structures for DISC-implementing programs to use.
+ */
+package disc.data;
---
+++ Dynamic Instruction-Scenario Components/src/disc/util/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Utilities for saving multiple Scenarios with/without a WaypointMap to a file,
+ * and reading it back.
+ */
+package disc.util;
